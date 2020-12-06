671dbe22591796b5b6a942c436ebacccedcc8d3d
nikoloz kvaratskhelia <nkvar12@gmail.com>
2020-12-06 13:27:57 +0200

    initial commit

---
+++ src/test/java/com/example/universe/simulator/entityservice/EntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universe.simulator.entityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
