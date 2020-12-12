fb010cd54cccf69f696e2045c698f81c12448c9f
nikoloz kvaratskhelia <nkvar12@gmail.com>
2020-12-12 18:35:20 +0200

    added db model template entities

--- src/main/java/com/example/universe/simulator/entityservice/entities/Galaxy.java
+++ src/main/java/com/example/universe/simulator/entityservice/entities/Galaxy.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Entity
 @Getter @Setter
-public class Star extends SpaceEntity {
+public class Galaxy extends SpaceEntity {
 
-    @ManyToOne
-    private Galaxy galaxy;
+    @OneToMany(mappedBy = "galaxy")
+    private Set<Star> stars;
 }
---
+++ src/main/java/com/example/universe/simulator/entityservice/entities/SpaceEntity.java
@@ -0,0 +1,27 @@
+package com.example.universe.simulator.entityservice.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+import java.util.UUID;
+
+/**
+ * parent for all space objects
+ */
+@MappedSuperclass
+@Getter @Setter
+public abstract class SpaceEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+
+    @Version
+    private long version;
+}
