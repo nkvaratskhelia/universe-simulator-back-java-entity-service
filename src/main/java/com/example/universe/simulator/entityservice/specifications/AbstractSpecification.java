5c65a2c6ea719c938c60db852b427099b854872c
nikoloz kvaratskhelia <nkvar12@gmail.com>
2021-01-10 09:52:04 +0200

    added galaxy filtering - added final modifier to methods in various abstract classes

--- src/main/java/com/example/universe/simulator/entityservice/specifications/AbstractSpecification.java
+++ src/main/java/com/example/universe/simulator/entityservice/specifications/AbstractSpecification.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.domain.Specification;
 
 abstract class AbstractSpecification<T> {
 
-    Specification<T> like(String property, String value) {
+    final Specification<T> like(String property, String value) {
         return (root, query, criteriaBuilder) ->
                 criteriaBuilder.like(
                         criteriaBuilder.lower(root.get(property)),
