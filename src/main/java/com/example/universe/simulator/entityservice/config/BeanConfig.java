e58395e2c4a199e504d1f57eb3586cc211418987
nikoloz kvaratskhelia <nkvar12@gmail.com>
2021-04-14 08:13:47 +0300

    changed creation of applicationTaskExecutor - changed creation of applicationTaskExecutor to use TaskExecutorBuilder - added additional reference to spring boot documentation for applicationTaskExecutor bean

--- src/main/java/com/example/universe/simulator/entityservice/config/BeanConfig.java
+++ src/main/java/com/example/universe/simulator/entityservice/config/BeanConfig.java
@@ -1,24 +1,23 @@
 package com.example.universe.simulator.entityservice.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.task.TaskExecutorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class BeanConfig {
 
     //https://docs.spring.io/spring-framework/docs/current/reference/html/web.html#mvc-ann-async-configuration-spring-mvc
+    //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-task-execution-scheduling
     @Bean
-    AsyncTaskExecutor applicationTaskExecutor() {
-        ThreadPoolTaskExecutor result = new ThreadPoolTaskExecutor();
-
+    AsyncTaskExecutor applicationTaskExecutor(TaskExecutorBuilder taskExecutorBuilder) {
         int numProcessors = Runtime.getRuntime().availableProcessors();
-        result.setCorePoolSize(numProcessors);
-        result.setMaxPoolSize(numProcessors);
-
-        return result;
+        return taskExecutorBuilder
+            .corePoolSize(numProcessors)
+            .maxPoolSize(numProcessors)
+            .build();
     }
 
     @Bean
