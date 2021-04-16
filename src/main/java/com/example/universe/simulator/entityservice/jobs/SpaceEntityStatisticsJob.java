5c6ebda21b64834df75b0d9a3543eaecf433f2a5
nkvar12 <nkvar12@gmail.com>
2021-04-16 22:23:08 +0300

    added space entity statistics job - changed space entity statistics job cron to run daily - implemented logStatistics method in SpaceEntityStatisticsJob - added EventProcessorTest - made EventProcessorTest private

--- src/main/java/com/example/universe/simulator/entityservice/jobs/SpaceEntityStatisticsJob.java
+++ src/main/java/com/example/universe/simulator/entityservice/jobs/SpaceEntityStatisticsJob.java
@@ -1,6 +1,9 @@
 package com.example.universe.simulator.entityservice.jobs;
 
-import com.example.universe.simulator.entityservice.services.GalaxyService;
+import com.example.universe.simulator.entityservice.repositories.GalaxyRepository;
+import com.example.universe.simulator.entityservice.repositories.MoonRepository;
+import com.example.universe.simulator.entityservice.repositories.PlanetRepository;
+import com.example.universe.simulator.entityservice.repositories.StarRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,12 +12,20 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-class SpaceEntityStatisticsJob {
+public class SpaceEntityStatisticsJob {
 
-    private final GalaxyService galaxyService;
+    private final GalaxyRepository galaxyRepository;
+    private final StarRepository starRepository;
+    private final PlanetRepository planetRepository;
+    private final MoonRepository moonRepository;
 
     @Scheduled(cron = "${app.scheduling.space-entity-statistics-job-cron}")
-    void createSpaceEntities() {
-
+    public void logStatistics() {
+        log.info("gathering space entity statistics...");
+        long numGalaxies = galaxyRepository.count();
+        long numStars = starRepository.count();
+        long numPlanets = planetRepository.count();
+        long numMoons = moonRepository.count();
+        log.info("space entity statistics: galaxies [{}], stars [{}], planets [{}], moons [{}]", numGalaxies, numStars, numPlanets, numMoons);
     }
 }
---
+++ src/test/java/com/example/universe/simulator/entityservice/unit/jobs/SpaceEntityStatisticsJobTest.java
@@ -0,0 +1,44 @@
+package com.example.universe.simulator.entityservice.unit.jobs;
+
+import com.example.universe.simulator.entityservice.jobs.SpaceEntityStatisticsJob;
+import com.example.universe.simulator.entityservice.repositories.GalaxyRepository;
+import com.example.universe.simulator.entityservice.repositories.MoonRepository;
+import com.example.universe.simulator.entityservice.repositories.PlanetRepository;
+import com.example.universe.simulator.entityservice.repositories.StarRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+public class SpaceEntityStatisticsJobTest {
+
+    @Mock
+    private GalaxyRepository galaxyRepository;
+
+    @Mock
+    private StarRepository starRepository;
+
+    @Mock
+    private PlanetRepository planetRepository;
+
+    @Mock
+    private MoonRepository moonRepository;
+
+    @InjectMocks
+    private SpaceEntityStatisticsJob spaceEntityStatisticsJob;
+
+    @Test
+    void testLogStatistics() {
+        //when
+        spaceEntityStatisticsJob.logStatistics();
+        //then
+        then(galaxyRepository).should().count();
+        then(starRepository).should().count();
+        then(planetRepository).should().count();
+        then(moonRepository).should().count();
+    }
+}
