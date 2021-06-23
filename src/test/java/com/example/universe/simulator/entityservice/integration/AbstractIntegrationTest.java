805b212efcb4002d54263321bd53a2fd139313ba
nkvar12 <nkvar12@gmail.com>
2021-06-23 13:51:52 +0300

    added sending events to event service - fixed wildcard for jpamodelgen classes in jacocoExclusions in build.gradle - added testcontainers-bom in build.gradle - added org.testcontainers:rabbitmq dependency in build.gradle - removed spring.autoconfigure.exclude & server.ssl.enabled properties in bootstrap-test.properties - added rabbitmq properties in bootstrap-test.properties - added sending events to rabbitmq broker in EventProcessor - added RabbitMQContainer setup in AbstractIntegrationTest - added EventProcessorTest unit test - fixed test method name in EventPublisherTest

--- src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
@@ -3,7 +3,30 @@ package com.example.universe.simulator.entityservice.integration;
 import com.example.universe.simulator.entityservice.common.abstractions.AbstractMockMvcTest;
 import com.example.universe.simulator.entityservice.common.abstractions.AbstractSpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.RabbitMQContainer;
 
 @AbstractSpringBootTest
 @AutoConfigureMockMvc
-abstract class AbstractIntegrationTest extends AbstractMockMvcTest {}
+abstract class AbstractIntegrationTest extends AbstractMockMvcTest {
+
+    private static final RabbitMQContainer RABBITMQ_CONTAINER;
+
+    static {
+        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:management");
+        RABBITMQ_CONTAINER.start();
+    }
+
+    /**
+     * For networking info in TestContainers, check the following url:
+     * https://www.testcontainers.org/features/networking/
+     */
+    @DynamicPropertySource
+    private static void addTestContainersProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.rabbitmq.host", RABBITMQ_CONTAINER::getHost);
+        registry.add("spring.rabbitmq.port", RABBITMQ_CONTAINER::getAmqpPort);
+        registry.add("spring.rabbitmq.username", RABBITMQ_CONTAINER::getAdminUsername);
+        registry.add("spring.rabbitmq.password", RABBITMQ_CONTAINER::getAdminPassword);
+    }
+}
