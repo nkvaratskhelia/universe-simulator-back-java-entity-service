59c49dc62eac8d63d50f5880a27b9570925561ab
nkvaratskhelia <nkvar12@gmail.com>
2022-01-30 18:50:50 +0400

    updated app props - removed gnoreUnknownFields = false param in @ConfigurationProperties in AppProperties - removed logstashUrl field in AppProperties

--- src/main/java/com/example/universe/simulator/entityservice/config/AppProperties.java
+++ src/main/java/com/example/universe/simulator/entityservice/config/AppProperties.java
@@ -8,19 +8,13 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
-@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "app")
 @Validated
 @Getter @Setter
 public class AppProperties {
 
     @Valid
     private SchedulingProperties scheduling = new SchedulingProperties();
-
-    /**
-     * Logstash connection URL, in the form of host:port.
-     */
-    @NotBlank
-    private String logstashUrl;
 
     @Getter @Setter
     public static class SchedulingProperties {
