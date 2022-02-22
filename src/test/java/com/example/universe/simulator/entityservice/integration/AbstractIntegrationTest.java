08da74e2ed0b62f89f9541e116da6d3d2b544c66
nkvaratskhelia <nkvar12@gmail.com>
2022-02-22 14:55:13 +0400

    changes in AbstractIntegrationTest - reordering in AbstractIntegrationTest

--- src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
@@ -16,15 +16,15 @@ import org.testcontainers.containers.RabbitMQContainer;
 @RecordApplicationEvents
 abstract class AbstractIntegrationTest extends AbstractMockMvcTest {
 
-    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
     private static final RabbitMQContainer RABBITMQ_CONTAINER;
+    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
 
     @Autowired
     protected ApplicationEvents applicationEvents;
 
     static {
-        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:14.2");
         RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.9.13-management");
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:14.2");
 
         RABBITMQ_CONTAINER.start();
         POSTGRESQL_CONTAINER.start();
@@ -32,13 +32,13 @@ abstract class AbstractIntegrationTest extends AbstractMockMvcTest {
 
     @DynamicPropertySource
     private static void addTestContainersProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
-
         registry.add("spring.rabbitmq.host", RABBITMQ_CONTAINER::getHost);
         registry.add("spring.rabbitmq.port", RABBITMQ_CONTAINER::getAmqpPort);
         registry.add("spring.rabbitmq.username", RABBITMQ_CONTAINER::getAdminUsername);
         registry.add("spring.rabbitmq.password", RABBITMQ_CONTAINER::getAdminPassword);
+
+        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
     }
 }
