0a40e5881395c5052c15010fe684cccaa391a6a3
nkvaratskhelia <nkvar12@gmail.com>
2022-06-27 12:27:31 +0400

    optimized tests - added modelMapper field in AbstractMockMvcTest - removed modelMapper field in AbstractWebMvcTest - reworked GalaxyIntegrationTest

--- src/test/java/com/example/universe/simulator/entityservice/common/abstractions/AbstractMockMvcTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/common/abstractions/AbstractMockMvcTest.java
@@ -4,6 +4,7 @@ import com.example.universe.simulator.common.dtos.ErrorDto;
 import com.example.universe.simulator.entityservice.types.ErrorCodeType;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -23,6 +24,9 @@ public abstract class AbstractMockMvcTest {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected ModelMapper modelMapper;
 
     /*
      * Handles sync and async requests. For async requests, see
--- src/test/java/com/example/universe/simulator/entityservice/common/abstractions/AbstractWebMvcTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/common/abstractions/AbstractWebMvcTest.java
@@ -3,8 +3,6 @@ package com.example.universe.simulator.entityservice.common.abstractions;
 import com.example.universe.simulator.common.test.AbstractTest;
 import com.example.universe.simulator.entityservice.config.BeanConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -16,9 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AbstractTest
 @Import(BeanConfig.class)
 public abstract class AbstractWebMvcTest extends AbstractMockMvcTest {
-
-    @Autowired
-    protected ModelMapper modelMapper;
 
     protected final void verifyOkStatus(int status) {
         assertThat(status).isEqualTo(HttpStatus.OK.value());
