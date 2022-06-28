cadc9c3ed21d12306896dc64fb07be450ba5dfb9
nkvaratskhelia <nkvar12@gmail.com>
2022-06-28 08:21:20 +0400

    made properties injectable - added @Component in SchedulingProperties - added ignoreInvalidFields = true flag to @ConfigurationProperties in SchedulingProperties - updated common version to 1.3.4

--- src/main/java/com/example/universe/simulator/entityservice/config/SchedulingProperties.java
+++ src/main/java/com/example/universe/simulator/entityservice/config/SchedulingProperties.java
@@ -3,11 +3,13 @@ package com.example.universe.simulator.entityservice.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 
-@ConfigurationProperties(prefix = "app.scheduling")
+@ConfigurationProperties(prefix = "app.scheduling", ignoreInvalidFields = true)
+@Component
 @Validated
 @Getter @Setter
 public class SchedulingProperties {
