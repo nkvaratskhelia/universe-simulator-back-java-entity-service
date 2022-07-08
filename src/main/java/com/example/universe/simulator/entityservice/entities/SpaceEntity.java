c492da28c3df844cf72a7040a3c1a71d72f55596
nkvaratskhelia <nkvar12@gmail.com>
2022-07-08 13:39:14 +0400

    removed AbstractEntity - moved fields from AbstractEntity to SpaceEntity - SpaceEntity no longer extends AbstractEntity - removed "callSuper = true" param from @EqualsAndHashCode in SpaceEntity - removed AbstractEntity

--- src/main/java/com/example/universe/simulator/entityservice/entities/SpaceEntity.java
+++ src/main/java/com/example/universe/simulator/entityservice/entities/SpaceEntity.java
@@ -7,14 +7,25 @@ import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+import java.util.UUID;
 
 @MappedSuperclass
 @Getter @Setter
 @NoArgsConstructor
 @SuperBuilder
-@EqualsAndHashCode(callSuper = true)
-public abstract class SpaceEntity extends AbstractEntity {
+@EqualsAndHashCode
+public abstract class SpaceEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Version
+    private long version;
 
     @Column(nullable = false, unique = true)
     private String name;
