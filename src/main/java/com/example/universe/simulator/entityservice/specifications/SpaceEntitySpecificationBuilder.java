a529927ab7a979208739bf27d6b51fa5bba2ce95
nkvaratskhelia <nkvar12@gmail.com>
2022-07-08 22:17:19 +0400

    renamed AbstractSpecification - renamed AbstractSpecification to SpecificationUtils

--- src/main/java/com/example/universe/simulator/entityservice/specifications/SpaceEntitySpecificationBuilder.java
+++ src/main/java/com/example/universe/simulator/entityservice/specifications/SpaceEntitySpecificationBuilder.java
@@ -19,7 +19,7 @@ abstract class SpaceEntitySpecificationBuilder<E extends SpaceEntity, F extends 
 
     private Specification<E> nameLike(String name) {
         return StringUtils.hasText(name)
-               ? AbstractSpecification.like(SpaceEntity_.NAME, name)
+               ? SpecificationUtils.like(SpaceEntity_.NAME, name)
                : null;
     }
 
--- src/test/java/com/example/universe/simulator/entityservice/unit/specifications/SpecificationUtilsTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/unit/specifications/SpecificationUtilsTest.java
@@ -1,17 +1,17 @@
 package com.example.universe.simulator.entityservice.unit.specifications;
 
-import com.example.universe.simulator.entityservice.specifications.AbstractSpecification;
+import com.example.universe.simulator.entityservice.specifications.SpecificationUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.jpa.domain.Specification;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AbstractSpecificationTest {
+class SpecificationUtilsTest {
 
     @Test
     void testLike() {
         // when
-        Specification<?> result = AbstractSpecification.like("property", "value");
+        Specification<?> result = SpecificationUtils.like("property", "value");
         // then
         assertThat(result).isNotNull();
     }
