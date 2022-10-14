14c163a52c733f98755332b0fec814fc90f0bbd7
nkvaratskhelia <nkvar12@gmail.com>
2022-10-14 09:12:48 +0200

    updated rabbitmq - updated rabbitmq version to 3.11.1 in AbstractIntegrationTest

--- src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
@@ -35,7 +35,7 @@ abstract class AbstractIntegrationTest extends AbstractMockMvcTest {
     protected CacheManager cacheManager;
 
     static {
-        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.11.0-management");
+        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.11.1-management");
         REDIS_CONTAINER = new GenericContainer<>("redis:7.0.5").withExposedPorts(6379);
         POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:14.5");
 
