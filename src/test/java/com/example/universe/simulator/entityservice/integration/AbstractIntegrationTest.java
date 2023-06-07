ab4bdb63e9cd1ad36bc02422ff2fe5ac2a7df6e1
nkvaratskhelia <nkvar12@gmail.com>
2023-06-07 11:31:58 +0400

    added spring-boot-testcontainers support - added spring-boot-testcontainers dependency - replaced @DynamicPropertySource with @ServiceConnection-s in AbstractIntegrationTest - removed @AutoConfigureTestDatabase in AbstractSmokeTest

--- src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
@@ -5,9 +5,8 @@ import com.example.universe.simulator.common.test.AbstractSpringBootTest;
 import com.example.universe.simulator.entityservice.common.abstractions.AbstractMockMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.cache.CacheManager;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.event.ApplicationEvents;
 import org.springframework.test.context.event.RecordApplicationEvents;
 import org.testcontainers.containers.GenericContainer;
@@ -25,8 +24,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RecordApplicationEvents
 abstract class AbstractIntegrationTest extends AbstractMockMvcTest {
 
+    @ServiceConnection
     private static final RabbitMQContainer RABBITMQ_CONTAINER;
+
+    @ServiceConnection
     private static final GenericContainer<?> REDIS_CONTAINER;
+
+    @ServiceConnection
     private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
 
     @Autowired
@@ -41,21 +45,6 @@ abstract class AbstractIntegrationTest extends AbstractMockMvcTest {
         POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:15.3");
 
         Startables.deepStart(RABBITMQ_CONTAINER, REDIS_CONTAINER, POSTGRESQL_CONTAINER).join();
-    }
-
-    @DynamicPropertySource
-    private static void addTestContainersProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.rabbitmq.host", RABBITMQ_CONTAINER::getHost);
-        registry.add("spring.rabbitmq.port", RABBITMQ_CONTAINER::getAmqpPort);
-        registry.add("spring.rabbitmq.username", RABBITMQ_CONTAINER::getAdminUsername);
-        registry.add("spring.rabbitmq.password", RABBITMQ_CONTAINER::getAdminPassword);
-
-        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
-        registry.add("spring.data.redis.port", REDIS_CONTAINER::getFirstMappedPort);
-
-        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
     }
 
     protected final void verifyEventsByType(Map<String, Long> expected) {
--- src/test/java/com/example/universe/simulator/entityservice/smoke/AbstractSmokeTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/smoke/AbstractSmokeTest.java
@@ -1,8 +1,6 @@
 package com.example.universe.simulator.entityservice.smoke;
 
 import com.example.universe.simulator.common.test.AbstractSpringBootTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @AbstractSpringBootTest
-@AutoConfigureTestDatabase
 abstract class AbstractSmokeTest {}
