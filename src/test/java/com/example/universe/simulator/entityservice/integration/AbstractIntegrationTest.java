f7a2727494f6fc2ca333602fbb3425321211955f
nkvaratskhelia <nkvar12@gmail.com>
2026-03-11 11:15:26 +0400

    updates

--- src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/entityservice/integration/AbstractIntegrationTest.java
@@ -40,9 +40,9 @@ abstract class AbstractIntegrationTest extends AbstractMockMvcTest {
     protected CacheManager cacheManager;
 
     static {
-        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:4.2.3-management");
-        REDIS_CONTAINER = new RedisContainer("redis:8.6.0");
-        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:18.2");
+        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:4.2.4-management");
+        REDIS_CONTAINER = new RedisContainer("redis:8.6.1");
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:18.3");
 
         Startables.deepStart(RABBITMQ_CONTAINER, REDIS_CONTAINER, POSTGRESQL_CONTAINER).join();
     }
